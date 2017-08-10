66039af60c8ae8d49a6262a0be7d0d5ae590ce6d
Unknown <iliasilisparrow@gmail.com>
2017-08-10 13:55:38 +0200

    Separation fichier reussie. Projet fonctionnel

--- Carte Ilias/Import Carte/Leaflet fonc/js/evenement.js
+++ Carte Ilias/Import Carte/Leaflet fonc/js/evenement.js
@@ -1,1 +1,15 @@
- 
+	var nom='';
+	var coordonnee='';
+		function onEachFeature(feature, layer) {
+			layer.bindPopup(feature.properties.name);
+			layer.on('click', function (e){
+											nom = feature.properties.name;
+											coordonnee= feature.geometry.coordinates;
+											//window.alert(nom);
+											//window.alert(coordonnee);
+										  });
+	}
+
+	L.geoJSON(test, {
+		onEachFeature: onEachFeature
+	}).addTo(map); 
